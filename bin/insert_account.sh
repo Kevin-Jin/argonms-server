ce76451be6ac6ed9613b625a56b9cebf4e2ecd54
geospiza <geospiza@protonmail.com>
2020-11-02 22:53:20 -0800

    Update insert_account with specific fields

--- bin/insert_account.sh
+++ bin/insert_account.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-echo 'insert into accounts 
-values (0, "testing", "testing", "salt", "1234", 10, NULL, 3, 0, NULL, NULL, 4, 0, 1);
+echo 'INSERT INTO accounts (id, name, password, salt, pin, gm)
+VALUES (0, "testing", "testing", "salt", "1234", 1);
 ' | mysql -h db --password=testing argonms
